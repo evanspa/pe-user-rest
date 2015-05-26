6cc3588017b7feefd064001af488881a7471a07f
Paul Evans <evansp2@gmail.com>
2015-05-26 11:33:01 -0400

    Version 0.0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-user-rest "0.0.7-SNAPSHOT"
+(defproject pe-user-rest "0.0.7"
   :description "A Clojure library encapsulating an abstraction modeling a user within a REST API."
   :url "https://github.com/evanspa/pe-user-rest"
   :license {:name "MIT"
